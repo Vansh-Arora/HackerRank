783a457020f7b9eb9bd50f440d7ca8a15385bdc0
Vansh <vansharora2929@gmail.com>
2020-09-13 01:21:16 +0530

    New logic coded

--- RandomCodes/Sherlock_and_Permutations/solution.py
+++ RandomCodes/Sherlock_and_Permutations/solution.py
@@ -1,3 +1,9 @@
+def result(n0, n1):
+    result = 1
+    for i in range((n1 + 1), (n0 + n1 + 1)):
+        result = (result * i) // (i - n1)
+    print(result)
 n = int(input())
 for i in range(n):
-    n0,n1 = map(int,input().split())
+    m,n = map(int,input().split())
+    result(m,n-1)
