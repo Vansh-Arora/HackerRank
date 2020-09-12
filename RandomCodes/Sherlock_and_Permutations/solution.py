fb096a1f300683cb4ca8f9a45a59ac04d40eb927
Vansh <vansharora2929@gmail.com>
2020-09-13 01:22:26 +0530

    retuened answer modulo 10^9 + 7

--- RandomCodes/Sherlock_and_Permutations/solution.py
+++ RandomCodes/Sherlock_and_Permutations/solution.py
@@ -2,7 +2,7 @@ def result(n0, n1):
     result = 1
     for i in range((n1 + 1), (n0 + n1 + 1)):
         result = (result * i) // (i - n1)
-    print(result)
+    print(result % 1000000007)
 n = int(input())
 for i in range(n):
     m,n = map(int,input().split())
