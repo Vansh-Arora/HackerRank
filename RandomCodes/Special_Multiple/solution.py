b737b7f000737ab6c5a2eb23ed1c96bfb3d3efd0
Vansh <vansharora2929@gmail.com>
2020-09-14 02:19:34 +0530

    Solution done

--- RandomCodes/Special_Multiple/solution.py
+++ RandomCodes/Special_Multiple/solution.py
@@ -1,3 +1,11 @@
+def SpecialMultiple(num):
+    i = 1
+    while( int(str(bin(i)[2:].replace("1","9"))) % num != 0):
+        i += 1
+    print(int(str(bin(i)[2:].replace("1","9"))))
+
+
 T = int(input())
 for i in range(T):
     num = int(input())
+    SpecialMultiple(num)
