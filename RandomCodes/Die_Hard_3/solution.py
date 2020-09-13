a1226447d25653e4a069616a5008dc1735517fac
Vansh <vansharora2929@gmail.com>
2020-09-14 04:39:32 +0530

    Solution Complete

--- RandomCodes/Die_Hard_3/solution.py
+++ RandomCodes/Die_Hard_3/solution.py
@@ -1,5 +1,15 @@
+import math
+def dh3(a,b,r):
+    if r<b or r<a:
+        if r % math.gcd(a,b) == 0:
+            print("YES")
+        else:
+            print("NO")
+    else:
+        print("NO")
 T = int(input())
 assert(1<=T<=100)
 
 for i in range(T):
     a,b,r = map(int,input().split())
+    dh3(a,b,r)
