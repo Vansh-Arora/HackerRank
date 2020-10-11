012bc329328072a8c8e4ab23a12ed3b95f2cd51d
Vansh <vansharora2929@gmail.com>
2020-10-12 01:42:50 +0530

    Brute Force Method

--- Interview_Preparation-Kit/Arrays/NewYearChaos.py
+++ Interview_Preparation-Kit/Arrays/NewYearChaos.py
@@ -1,6 +1,6 @@
 # Problem Statement: https://www.hackerrank.com/challenges/new-year-chaos/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
 # find bribes
-
+# Brute Force Method (O(n^2))
 t = int(input())
 j = 0
 while j<t:
@@ -8,16 +8,12 @@ while j<t:
     n = int(input())
     a = list(map(int, input().rstrip().split()))
     bribes = 0
-    flag = 1
     for i in range(n):
-        if a[i] > i+1:
-            bribe = a[i] - (i+1)
-            if bribe > 2:
-                flag = 0
-                print("Too chaotic")
-                break
-            bribes = bribes + bribe
-        elif (i+1 - a[i] > 1):
-            bribes+=1
-    if(flag):
-        print(bribes)
+        if (a[i] - (i+1))>2:
+            bribes = "Too chaotic"
+            break
+        key = a[i]
+        for k in range(i+1,n):
+            if a[k] < key:
+                bribes += 1
+    print(bribes)
