413922bb0c3e92b88e934b66f92eedc7bfdaaa43
Vansh <vansharora2929@gmail.com>
2020-10-12 02:05:21 +0530

    Approach towards problem

--- Interview_Preparation-Kit/Arrays/NewYearChaos2.py
+++ Interview_Preparation-Kit/Arrays/NewYearChaos2.py
@@ -2,6 +2,7 @@
 # find bribes
 
 # Optimized O(n)
+# Max No. of times a person can bribe is 2
 
 t = int(input())
 j = 0
@@ -11,24 +12,23 @@ while j<t:
     a = list(map(int, input().rstrip().split()))
     bribes = 0
     flag = 1
+
     for i in range(n):
-        if (a[i] - (i+1))>2:
+        if (a[i] - (i+1))>2:                     # To check if person has bribed more than 2 people.
             bribes = "Too chaotic"
             flag = 0
             break
 
     if(flag):
         for i in range(n-1,-1,-1):
-            if a[i] != i+1:
-                if a[i-1] == i+1:
+            if a[i] != i+1:                       # Condition1: check if a person has bribed
+                if a[i-1] == i+1:                 # Condition2: check if the no. has bribed the person on his left
                     bribes += 1
-                    a[i-1],a[i] = a[i],a[i-1]
+                    a[i-1],a[i] = a[i],a[i-1]     #               if yes: swap persons
                 else:
-                    bribes += 2
-                    a[i-2] = a[i-1]
-                    a[i-1] = a[i]
-                    a[i] = i+1
+                    bribes += 2                   #               else: The person must have swiped 2 persons
+                    a[i-2] = a[i-1]               #               So, shift person at left toward one position left further
+                    a[i-1] = a[i]                 #                   shift current person to one position left
+                    a[i] = i+1                    #                    at current position put a[i] = i.
 
-
-    
     print(bribes)
